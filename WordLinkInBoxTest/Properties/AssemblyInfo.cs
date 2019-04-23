fa9efaa9ea39b8369c50ff4e71ab846a633cd1c2
qiang <1043360492@qq.com>
2019-04-23 16:03:47 +0800

    Add project files.

---
+++ WordLinkInBoxTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WordLinkInBoxTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WordLinkInBoxTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("42a6a8e1-305b-44e2-8f23-a5734bce695e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WordLinkInBoxTest/WordLinkInBoxTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+
+namespace WordLinkInBoxTest
+{
+    [TestClass]
+    public class WordLinkInBoxTest
+    {
+        private const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
+        private const string CalculatorAppId = "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App";
+        protected static WindowsDriver<WindowsElement> session;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            if (session == null)
+            {
+                // Create a new session to bring up an instance of the Word
+                DesiredCapabilities appCapabilities = new DesiredCapabilities();
+                appCapabilities.SetCapability("app", "C:\\Program Files (x86)\\Microsoft Office\\root\\Office16\\WINWORD.EXE");
+                appCapabilities.SetCapability("deviceName", "WindowsPC");
+
+                session = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), appCapabilities);
+                Assert.IsNotNull(session);
+
+                //Word start with a process WindowsElement
+                //When word loaded, that WindowsElement close and reopen a new WindowsElement.
+                //Here we recreate a session with new WindowsElement.
+                session.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1.5);
+                session = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), appCapabilities);
+            }
+        }
+
+        [TestMethod]
+        [DataRow("选项", "领英功能1", "取消")]
+        //[DataRow("Options", "Minus")]
+        public void TestLinkInBox(string optionName, string linkinBoxName, string cancelButtonName)
+        {
+            session.FindElementByName(optionName).Click();
+            try
+            {
+                session.FindElementByName(linkinBoxName);
+            }
+            catch (Exception)
+            {
+                throw new Exception("LinkIn box not found");
+            }
+            finally
+            {
+                //close option form
+                session.FindElementByName(cancelButtonName).Click();
+            }
+        }
+
+        [ClassCleanup]
+        public static void TearDown()
+        {
+            // Close the application and delete the session
+            if (session != null)
+            {
+                session.Quit();
+                session = null;
+            }
+        }
+    }
+}
